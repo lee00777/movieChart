834fc04c09bf192b45410b66c8ded02fba0a014e
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-07-07 16:55:25 +0900

    Fixed the content on a movie card

--- src/movie/MovieItem.js
+++ src/movie/MovieItem.js
@@ -1,43 +1,44 @@
 import React, { useRef, useState } from "react";
-import './MovieItem.css'
+import "./MovieItem.css";
 import { FcLikePlaceholder, FcLike } from "react-icons/fc";
 import { FiVideo } from "react-icons/fi";
 import { Card } from "materialize-css";
-import Numeral from 'numeral';
+import Numeral from "numeral";
 
 const MovieItem = ({ movie, onOpen, onLike }) => {
   const { rank, id, overview, title, poster, date, done, popularity, rating } = movie;
   return (
-    <div className="col s12 m6 l3 " >
-
+    <div className="col s12 m6 l3 ">
       <div className="card">
-       <img  className="card-image" src={`https://image.tmdb.org/t/p/w200/${poster}`} alt="a poster" />
+        <img className="card-image" src={`https://image.tmdb.org/t/p/w200/${poster}`} alt="a poster" />
         <div className="card-text">
           <h2>{title}</h2>
-          <p>Released : {date}</p>
+          <p>Release Date : {date}</p>
           <p>Rating : {rating}</p>
         </div>
         <div className="card-stats">
           <div className="stat">
             <div className="value">
-                <p>{rank}</p>
+              <p>{rank}</p>
             </div>
             <div className="type">Rank</div>
           </div>
           <div className="stat border">
             <div className="value">
-                <p onClick={() => onLike(id)}>
-                  <i>{done ? <FcLike /> : <FcLikePlaceholder  />}</i>
-                  &nbsp; {Numeral(Math.floor(popularity)).format(0, 0)}
-                </p>
+              <p onClick={() => onLike(id)}>
+                <i>{done ? <FcLike /> : <FcLikePlaceholder />}</i>
+                &nbsp; {Numeral(Math.floor(popularity)).format(0, 0)}
+              </p>
             </div>
             <div className="type">Likes</div>
           </div>
           <div className="stat">
-              <div className="value">
-                  <p>
-                    <i onClick={() => onOpen(id)}><FiVideo /></i>
-                  </p>
+            <div className="value">
+              <p>
+                <i onClick={() => onOpen(id)}>
+                  <FiVideo />
+                </i>
+              </p>
             </div>
             <div className="type">Trailer</div>
           </div>
